28c6ad8cf909113fdae1441cb80ebd883f2b0360
Gustice <jakob.ger@gmail.com>
2022-11-19 18:00:38 +0100

    tests: unit test for version and welcome-message working

--- Firmware/test/mocks/CdcDummy.h
+++ Firmware/test/mocks/CdcDummy.h
@@ -1,18 +1,54 @@
 #pragma once
 
-// #include "Stream.h"
+#include "Stream.h"
+#include <queue>
+#include <vector>
 
-// class CdcDummy : public Stream {
-//   public:
-//     void begin(void);
-    
-//     virtual int available(void) { return 0;}
-//     virtual int availableForWrite(void) { return 0;}
-//     virtual int peek(void) { return 0;}
-//     virtual int read(void) { return 0;}
-//     virtual size_t readBytes(char *buffer, size_t length) {return length;} 
-//     virtual size_t readBytesUntil(char terminator, char *buffer, size_t length) {return length;}
-//     virtual void flush(void) {};
-//     virtual size_t write(uint8_t) {return 1;}
-//     virtual size_t write(const uint8_t *buffer, size_t size) { return size;}
-// };
+class CdcDummy : public Stream {
+  public:
+    std::string OutBuffer;
+    std::queue<std::string> OutQueue;
+    std::string ReadOutputBuffer(void) {
+        if (OutQueue.size() != 0) {
+            auto msg = OutQueue.front();
+            OutQueue.pop();
+            return msg;
+        }
+
+        std::string msg(OutBuffer);
+        OutBuffer = "";
+        return msg;
+    }
+
+    void begin(void) {}
+    virtual int available(void) override { return 0; }
+    virtual int peek(void) override { return 0; }
+    virtual int read(void) override { return 0; }
+    virtual size_t readBytes(char *buffer, size_t length) override { return length; }
+    virtual size_t readBytesUntil(char terminator, char *buffer, size_t length) override {
+        return length;
+    }
+    virtual void flush(void) override{};
+
+    virtual size_t write(uint8_t c) override {
+        auto s = (char)c;
+        if (s == '\r')
+          return 1;
+
+        if (s == '\n') {
+            OutQueue.push(OutBuffer);
+            OutBuffer = "";
+        } else {
+            OutBuffer.push_back((char)c);
+        }
+        return 1;
+    }
+
+    virtual size_t write(const uint8_t *buffer, size_t size) override {
+        for (size_t i = 0; i < size; i++) {
+            write(buffer[i]);
+        }
+
+        return size;
+    }
+};
--- Firmware/test/test_Shell/ShellTest.cpp
+++ Firmware/test/test_Shell/ShellTest.cpp
@@ -1,55 +1,8 @@
-// #include "Shell.h"
-// #include <Arduino.h>
-
-// // #include "mocks/CdcDummy.h"
-// #include <unity.h>
-
-// void setUp(void) {}
-// void tearDown(void) {}
-
-// // CdcDummy serial;
-// DeviceState _device{
-//     .Visualization = VisualizationSate::Startup,
-//     .ButtonState = Button::State::Idle,
-// };
-
-// void SetColor(const Color &color) {
-//     // _shell.PrintResponse("Setting Color");
-//     // _pixelRing.SetEffect(macIdleAll, &color);
-// }
-// void SetScene(VisualizationSate scene) {
-//     // _shell.PrintResponse("Setting Effect");
-//     // const Scene &s = Scenes.at(scene);
-//     // _pixelRing.SetEffect(s.Effect, &s.color);
-// }
-
-// // Shell _shell(serial, _device, SetColor, SetScene);
-
-// void setup() {
-//     SerialUSB.begin();
-// }
-
-// void test_dummy(void)
-// {
-//     // _shell.PrintWelcome();
-//     // _shell.SendButtonEvent(Button::State::Pressed);
-//     TEST_ASSERT_EQUAL(1, 1);
-// }
-
-// void loop() {
-//     UNITY_BEGIN();
-//     RUN_TEST(test_dummy);
-//     UNITY_END(); // stop unit testing
-
-//     while (1)
-//     {
-//         // if (SerialUSB.peek() == Shell::EscapeChar)
-//         //     _shell.ConsumeSymbol(SerialUSB.read());
-//     }
-// }
-
 #include <Arduino.h>
 #include <unity.h>
+#include "Shell.h"
+#include "../mocks/CdcDummy.h"
+#include <string>
 
 extern "C" {
 int main();
@@ -58,15 +11,53 @@ void setup();
 void loop();
 }
 
-void setUp(void) {}
+CdcDummy serial;
+DeviceState _device{
+    .Visualization = VisualizationSate::Startup,
+    .ButtonState = Button::State::Idle,
+};
 
-void tearDown(void) {}
+void SetColor(const Color &color) {
+    // _shell.PrintResponse("Setting Color");
+    // _pixelRing.SetEffect(macIdleAll, &color);
+}
+void SetScene(VisualizationSate scene) {
+    // _shell.PrintResponse("Setting Effect");
+    // const Scene &s = Scenes.at(scene);
+    // _pixelRing.SetEffect(s.Effect, &s.color);
+}
 
-void test_led_builtin_pin_number(void) {}
+Shell _shell(serial, _device, SetColor, SetScene);
 
-void test_led_state_high(void) { TEST_ASSERT_EQUAL(1, 1); }
 
-void test_led_state_low(void) { TEST_ASSERT_EQUAL(1, 1); }
+void SendString(const std::string &send) {
+  for (size_t i = 0; i < send.size(); i++) {
+    _shell.ConsumeSymbol(send[i]);
+  }
+  _shell.ConsumeSymbol('\n');
+}
+
+void SendStringWithEcho(const std::string &send) {
+  static std::string prompt("$ >");
+  for (size_t i = 0; i < send.size(); i++) {
+    _shell.ConsumeSymbol(send[i]);
+  }
+  _shell.ConsumeSymbol('\n');
+  TEST_ASSERT_EQUAL_STRING(send.c_str(), serial.ReadOutputBuffer().substr(prompt.size()).c_str());
+}
+
+void CheckHelloMessage(void) {
+  _shell.PrintWelcome();
+  TEST_ASSERT_EQUAL_STRING("** Magic Button Shell **", serial.ReadOutputBuffer().c_str());
+  TEST_ASSERT_EQUAL_STRING("* Get Help by type ?-Symbol", serial.ReadOutputBuffer().c_str());
+  TEST_ASSERT_EQUAL(0, serial.OutQueue.size());
+}
+
+void ReadingVersion(void) {
+  SendStringWithEcho("Get Version:");
+  auto out =  serial.ReadOutputBuffer();
+  TEST_ASSERT_EQUAL_STRING("<TestButton T0.5", out.c_str());
+}
 
 void setup() {
     pinMode(PC13, OUTPUT);
@@ -77,15 +68,15 @@ void setup() {
 
 void loop() {
     UNITY_BEGIN();
-    RUN_TEST(test_led_builtin_pin_number);
-    for (unsigned int i = 0; i < 5; i++) {
-        RUN_TEST(test_led_state_high);
-        delay(10);
-        RUN_TEST(test_led_state_low);
-        delay(10);
-    }
+
+    RUN_TEST(CheckHelloMessage);
+    RUN_TEST(ReadingVersion);
+
     UNITY_END(); // stop unit testing
 
     while (1) {
     }
 }
+
+void setUp(void) {}
+void tearDown(void) {}
